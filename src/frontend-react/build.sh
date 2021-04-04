8c3d9e8902227889912369eac692dce0ca777ad9
Ty Eggen <tycho.eggen@gmail.com>
2021-04-03 22:59:19 -0700

    Changed build.sh to sync build files to esp32/controller/data

--- src/frontend-react/build.sh
+++ src/frontend-react/build.sh
@@ -2,18 +2,12 @@
 
 cd $(dirname $(realpath $0))
 
+npm install
+
 echo "Cleaning up build..."
 rm -fR build/*
 
 echo "Building static production build..."
 npm run build
 
-echo "Fixing paths..."
-echo ""
-echo "Fixing paths in chunks..."
-find build/static/ -type f -name '*.chunk.*' -exec perl -p -i -e 's/(\.chunk\.)(css\.map|js\.map|css|js)/$1$2$3.gz/g' {} \;
-echo "Fixing paths in top layer files..."
-find build/ -maxdepth 1 -type f -exec perl -p -i -e 's/(\.chunk\.)(css\.map|js\.map|css|js)/$1$2$3.gz/g' {} \;
-
-echo "Compressing build files..."
-find build/static/ -type f | xargs gzip -9
+rsync -avz ./build/ ../esp32/controller/data/

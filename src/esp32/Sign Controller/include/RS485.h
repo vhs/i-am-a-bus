bb8aae6f1e2a4e7f1e4ac2b41212c0033caa1b41
Ty Eggen <tycho.eggen@gmail.com>
2021-04-03 19:38:13 -0700

    Skeleton RS485 wrapper

---
+++ src/esp32/Sign Controller/include/RS485.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "platform.h"
+
+#include "config.h"
+
+class RS485
+{
+public:
+    String receiveFrame()
+    {
+        // Do stuff
+    }
+
+    bool sendFrame(String frame)
+    {
+        // Do stuff
+    }
+};

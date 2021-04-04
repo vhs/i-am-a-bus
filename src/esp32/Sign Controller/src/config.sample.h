05618ed0da8b3acab137ee5ffd4adadd62e33213
Ty Eggen <tycho.eggen@gmail.com>
2021-04-03 19:28:35 -0700

    Sample config.h

---
+++ src/esp32/Sign Controller/src/config.sample.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define PIN_RS485_RX 18
+#define PIN_RS485_TX 19
+#define PIN_RS485_EN 26 // RX=Low, TX=High
+
+#define PIN_LED_GREEN 4
+#define PIN_LED_RED 23
+
+#define PIN_ROT_A 14
+#define PIN_ROT_B 27
+#define PIN_ROT_SW 13
+
+// We actually use hardware defaults for I2C, but defined here for completeness
+#define PIN_I2C_SDA 21
+#define PIN_I2C_SCL 22

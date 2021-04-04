18c869eeb6185dd0bbad39d0fcee825c45afb152
Ty Eggen <tycho.eggen@gmail.com>
2021-04-04 00:12:16 -0700

    util.h (for writeConfig)

---
+++ src/esp32/controller/include/util.h
@@ -0,0 +1,30 @@
+#include <FS.h>
+#include <SPIFFS.h>
+
+String dirtyConfig = "";
+
+void writeConfig()
+{
+    Serial.printf("Writing config...\n");
+
+    File file = SPIFFS.open("/config.json", FILE_WRITE);
+
+    yield();
+
+    if (!file)
+    {
+        Serial.println("- failed to open config.json for writing");
+        return;
+    }
+
+    if (file.print(dirtyConfig.c_str()))
+        Serial.println("- config.json written");
+    else
+        Serial.println("- write failed");
+
+    yield();
+
+    file.close();
+
+    dirtyConfig = "";
+}

194f297a84f804f6e5f1f34401cce0121091b7e5
Ty Eggen <tycho.eggen@gmail.com>
2022-03-03 13:15:21 -0800

    src/frontend-react: improved dev setupProxy.sample.js

--- src/frontend-react/src/setupProxy.sample.js
+++ src/frontend-react/src/setupProxy.sample.js
@@ -4,8 +4,9 @@
 
 const ESPUrl = 'http://192.168.1.123'
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-    app.use('/api', createProxyMiddleware({ target: ESPUrl, changeOrigin: true, }))
+  app.use('/api', createProxyMiddleware({ target: ESPUrl, changeOrigin: true }))
+  app.use('/config.json', createProxyMiddleware({ target: ESPUrl, changeOrigin: true }))
 }

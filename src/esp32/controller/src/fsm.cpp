0d84442656e5a858aea572b52e484a6c1c7a34f7
Ty Eggen <tycho.eggen@gmail.com>
2022-03-04 23:35:45 -0800

    esp32-controller: various - config default screensaver - refactored FSM names

--- src/esp32/controller/src/fsm.cpp
+++ src/esp32/controller/src/fsm.cpp
@@ -2,10 +2,10 @@
 
 /**
  * FSM States:
- *  Boot - Boot up, sends init code to sign, auto falls to rest
- *  Rest - Resting loop. Rotary encoder adjusts brightness.
- *  Select - Selecting the message or select "program" to move into that state, control with rotary encoder. Times out back to rest.
- *  Program - Serves up network to join with phone, serves up webpage to modify stored codes. Times out back to rest.
+ *  Boot - Boot up, sends init code to sign, transitions to rest
+ *  Rest - Resting loop.
+ *  Menu - Menu loop. Rotary encoder selects option. Times out to rest.
+ *  Select - Selecting the message or select "program" to move into that state, control with rotary encoder. Click to select. Times out to rest.
  */
 
  // fsm state functions
@@ -59,7 +59,7 @@ void FSM_STATE_Rest_loop()
     if (checkButton())
     {
         Serial.println("REST: Button pressed");
-        fsm.trigger(FSM_TRIGGER_MENU);
+        fsm.trigger(FSM_TRIGGER_MAIN_MENU);
     }
 
     handleTextUpdate(&displayMainScreen);
@@ -70,13 +70,13 @@ void FSM_STATE_Rest_stop()
     Serial.println("Leaving Rest state");
 }
 
-void FSM_STATE_Menu_start()
+void FSM_STATE_Main_Menu_start()
 {
     Serial.println("Entering Menu State");
     displayMainMenu();
 }
 
-void FSM_STATE_Menu_loop()
+void FSM_STATE_Main_Menu_loop()
 {
     int menuAction = handleMainMenu();
 
@@ -86,18 +86,18 @@ void FSM_STATE_Menu_loop()
     }
     else if (menuAction == MENU_ACTION_SELECT)
     {
-        fsm.trigger(FSM_TRIGGER_SELECT);
+        fsm.trigger(FSM_TRIGGER_SELECT_MENU);
     }
 
     handleTextUpdate(&displayMainMenu);
 }
 
-void FSM_STATE_Menu_stop()
+void FSM_STATE_Main_Menu_stop()
 {
     Serial.println("Leaving Menu State");
 }
 
-void FSM_STATE_Select_start()
+void FSM_STATE_Select_Menu_start()
 {
     Serial.println("Entering Select state");
     clearScreen();
@@ -105,14 +105,14 @@ void FSM_STATE_Select_start()
     displaySelectMenu();
 }
 
-void FSM_STATE_Select_loop()
+void FSM_STATE_Select_Menu_loop()
 {
 
     int menuAction = handleSelectMenu();
 
     if (menuAction == MENU_ACTION_EXIT)
     {
-        fsm.trigger(FSM_TRIGGER_MENU);
+        fsm.trigger(FSM_TRIGGER_MAIN_MENU);
     }
     else
         if (menuAction != MENU_ACTION_IDLE) {
@@ -122,26 +122,26 @@ void FSM_STATE_Select_loop()
     handleTextUpdate(&displaySelectMenu);
 }
 
-void FSM_STATE_Select_stop()
+void FSM_STATE_Select_Menu_stop()
 {
     Serial.println("Leaving Select State");
 }
 
 FunctionState state_rest(&FSM_STATE_Rest_start, &FSM_STATE_Rest_loop, &FSM_STATE_Rest_stop);
-FunctionState state_menu(&FSM_STATE_Menu_start, &FSM_STATE_Menu_loop, &FSM_STATE_Menu_stop);
-FunctionState state_select(&FSM_STATE_Select_start, &FSM_STATE_Select_loop, &FSM_STATE_Select_stop);
+FunctionState state_main_menu(&FSM_STATE_Main_Menu_start, &FSM_STATE_Main_Menu_loop, &FSM_STATE_Main_Menu_stop);
+FunctionState state_select_menu(&FSM_STATE_Select_Menu_start, &FSM_STATE_Select_Menu_loop, &FSM_STATE_Select_Menu_stop);
 
 void FSM_Init() {
     // Transition from boot to rest
     fsm.add_transition(&state_boot, &state_rest, FSM_TRIGGER_REST, nullptr);
 
     // Transition from rest to menu and back
-    fsm.add_transition(&state_rest, &state_menu, FSM_TRIGGER_MENU, nullptr);
-    fsm.add_transition(&state_menu, &state_rest, FSM_TRIGGER_REST, nullptr);
+    fsm.add_transition(&state_rest, &state_main_menu, FSM_TRIGGER_MAIN_MENU, nullptr);
+    fsm.add_transition(&state_main_menu, &state_rest, FSM_TRIGGER_REST, nullptr);
 
     // Transition from menu to select and back
-    fsm.add_transition(&state_menu, &state_select, FSM_TRIGGER_SELECT, nullptr);
-    fsm.add_transition(&state_select, &state_menu, FSM_TRIGGER_MENU, nullptr);
+    fsm.add_transition(&state_main_menu, &state_select_menu, FSM_TRIGGER_SELECT_MENU, nullptr);
+    fsm.add_transition(&state_select_menu, &state_main_menu, FSM_TRIGGER_MAIN_MENU, nullptr);
 
     // Run FSM once to initialize
     fsm.run_machine();
